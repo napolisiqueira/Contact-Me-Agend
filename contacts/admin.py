2fc24f523c9134842c74a9d6bd04631c342a6f53
napolisiqueira <imfelipenapoli@gmail.com>
2025-06-27 22:13:59 -0300

    Add a custom configs in admin.ModelAdmin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+
+    list_display = ('id','first_name','last_name', 'phone',)
+    ordering = ('-id',)
+    list_filter = ('created_it',)
+    search_fields = ('id', 'first_name', 'last_name')
+    list_per_page = 10 
+    # list_editable =  ('first_name', 'last_name',)
--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,16 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, blank=True)
+    created_it = models.DateTimeField(default=timezone.now) 
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=50, blank=True)
+
+    def __str__(self) -> str:
+        return self.first_name
