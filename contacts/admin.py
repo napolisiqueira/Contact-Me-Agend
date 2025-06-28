b42ac8e2e60d51302a5213f11865b3adbc778e52
napolisiqueira <imfelipenapoli@gmail.com>
2025-06-27 22:16:29 -0300

    update last commit

--- contacts/admin.py
+++ contacts/admin.py
@@ -12,4 +12,6 @@ class ContactAdmin(admin.ModelAdmin):
     list_filter = ('created_it',)
     search_fields = ('id', 'first_name', 'last_name')
     list_per_page = 10 
-    # list_editable =  ('first_name', 'last_name',)
+    list_max_show_all = 200
+    list_editable =  ('first_name', 'last_name',)
+    list_display_links = ('id', 'phone',)
