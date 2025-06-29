c370e74389edb9824217ce778f939199e285af33
napolisiqueira <imfelipenapoli@gmail.com>
2025-06-29 00:37:37 -0300

    Organizing files views and begginnig connect  the back-end with fornt-end

--- contacts/views/contact_views.py
+++ contacts/views/contact_views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from contacts.models import Contact
+
 
 # Create your views here.
 def index(request):
+    contacts = Contact.objects.all()
+    context = {
+        "contacts": contacts,
+    }
+
     return render(
         request,
         'contacts\index.html',
+        context
     )
