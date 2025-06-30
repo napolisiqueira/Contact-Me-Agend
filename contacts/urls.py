2acac2e318c3eed532b242bbbd0924367d018510
napolisiqueira <imfelipenapoli@gmail.com>
2025-06-30 19:03:51 -0300

    add urls and view single-contact

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,4 +4,5 @@ from contacts import views
 app_name='contacts'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('<int:id>/', views.contact, name="contact"),
 ]
--- contacts/views/contact_views.py
+++ contacts/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contacts.models import Contact
+from django.http import Http404
 
 
 # Create your views here.
@@ -12,5 +13,19 @@ def index(request):
     return render(
         request,
         'contacts\index.html',
+        context
+    )
+
+def contact(request, id):
+    single_contact = get_object_or_404(
+        Contact.objects.filter(show=True),
+        pk=id)
+    context = {
+        "contacts": single_contact,
+    }
+
+    return render(
+        request,
+        'contacts\contact.html',
         context
     )
